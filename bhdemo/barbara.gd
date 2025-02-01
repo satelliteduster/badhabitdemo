35bd2fdf94c64e6c0d071a7125fea66d36ef2d48
satelliteduster <allmeiks@gmail.com>
2025-02-01 14:38:48 -0500

    updated
    - basic movement & animations fixed
    - added daniel (static object)
    - rudimentary scene [mini-maze]
    - collision working!

--- bhdemo/barbara.gd
+++ bhdemo/barbara.gd
@@ -1,20 +1,11 @@
 extends Sprite2D
 
-@export var speed = 2
-@onready var walking : AnimatedSprite2D = $AnimatedSprite2D
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	position.x = 0
-	position.y = 0
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_left"):
-		position.x -= 5
-	if Input.is_action_pressed("ui_right"):
-		position.x += 5
-	if Input.is_action_pressed("ui_up"):
-		position.y -= 5
-	if Input.is_action_pressed("ui_down"):
-		position.y += 5
+	pass
