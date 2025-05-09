333b2fbec2f0eef41461e135ada0dec0e4ae1740
satelliteduster <allmeiks@gmail.com>
2025-05-09 17:42:23 -0400

    final comp for now!!!

--- bhdemo/scripts/bhcontroller.gd
+++ bhdemo/scripts/bhcontroller.gd
@@ -5,6 +5,8 @@ extends Node
 @onready var barbara = $barbara
 
 func _ready():
+	#process_mode = Node.PROCESS_MODE_PAUSABLE
+	
 	#convent.hide() #hide convent floors on loading
 	#for child in convent.get_children():
 		#if child is CollisionShape2D or child is CollisionPolygon2D:
@@ -18,3 +20,15 @@ func _on_door_entered(body):
 func _scene_transition():
 	barbara.visible = true
 	barbara._on_spawn() # put barb in the new scene
+
+#re:pausing game v
+
+#func _pause_game():
+	#if Input.is_action_just_pressed("pause"):
+		#print("game paused.")
+		#get_tree().paused = true
+		#if Input.is_action_just_pressed("pause"):
+			#print("game unpaused.")
+			#get_tree().paused = false
+	#else:
+		#pass
